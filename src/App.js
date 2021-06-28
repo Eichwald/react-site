989fe9d597f62c89aef85ab20510ccf1b3a351c4
Eichwald <Eichwald6@hotmail.com>
2021-06-28 10:55:44 +0200

    Simple tabs layout
    A very simple tabs layout using materialUI and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from "react";
+import ReactDOM from 'react-dom';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const allTabs = ['/', '/albums'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs value={location.pathname}>
+                <Tab label="Item One" value="/" component={Link} to={allTabs[0]} />
+                <Tab label="Item Two" value="/albums" component={Link} to={allTabs[1]} />
+              </Tabs>
+              <Switch>
+                <Route path={allTabs[1]} render={() => <div>Tab 2</div>} />
+                <Route path={allTabs[0]} render={() => <div>Tab 1</div>} />
+              </Switch>
+            </Fragment>
+          )}
+        />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
